ee39ce0fcd0bf6fcdf3a4099c00ddada9039d564
yiwei <yiwei.21@ichat.sp.edu.sg>
2022-04-28 11:08:51 +0800

    ye

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-print("ahh");
+import math
+
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5,67, 32)")
+
+
+def get_user_input():
+    return [float(val) for val in input().split(",")]
+
+
+def calc_average(nums):
+    return sum(nums)/len(nums)
+
+
+def find_min_max(nums):
+    return [min(nums), max(nums)]
+
+
+def sort(nums):
+    return sorted(nums)
+
+
+def calc_median(nums):
+    if len(nums) % 2 == 0:
+        return (sort(nums)[math.floor(len(nums)/2)] + sort(nums)[math.floor(len(nums)/2)-1])/2
+    return sort(nums)[math.floor(len(nums)/2)]
+
+
+def main():
+    display_main_menu()
+    userInput = get_user_input()
+    print(userInput)
+    print(calc_average(userInput))
+    print(find_min_max(userInput))
+    print(sort(userInput))
+    print(calc_median(userInput))
+
+
+
+
+if __name__ == "__main__":
+    main()
